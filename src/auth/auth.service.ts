5142387cbcc093abf01f06b04e9b8bafd3eb2d32
Zygisvaldo <zygiodeze@gmail.com>
2023-09-11 12:19:34 +0200

    feat: simple auth added jwt

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './auth.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async register(username: string, password: string): Promise<void> {
+    const user = new User();
+    user.username = username;
+    user.password = bcrypt.hashSync(password, 8);
+    await this.usersRepository.save(user);
+  }
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersRepository.findOne({ where: { username } });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('*', (req, res, next) => {
-    throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-  });
+  // app.use('*', (req, res, next) => {
+  //   throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+  // });
   await app.listen(3000);
 }
 bootstrap();
